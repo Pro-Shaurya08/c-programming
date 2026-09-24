85041a9b44d028d70cfb73ba013b6aa52c454fde
Pro-Shaurya08 <shaurya27052008@gmail.com>
2026-09-24 19:18:04 +0530

    Add files via upload

---
+++ Practice_program_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name:Shaurya S Lokhande");
+    printf(" ");
+    printf("Department:School of Computing");
+    printf(" ");
+    printf("College:MIT-ADT");
+    return 0;
+}
